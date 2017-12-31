c64a72743e106bba47477239d9c3bbcbeb4518a4
Goran <goranmrd@gmail.com>
2017-12-31 14:13:35 +0100

    Update glitch.js

--- static/js/glitch.js
+++ static/js/glitch.js
@@ -1,5 +1,5 @@
 <script>
-				var canvas = document.getElementById('canvas')
+			var canvas = document.getElementById('canvas')
 			  , context = canvas.getContext('2d')
 			  , img = new Image()
 			  , w
